5313e81d8efa1b424dfa7fd1e346a734954e6f16
floleneko <dancausseflorian@gmail.com>
2016-05-11 10:14:24 +0200

    test de l'api

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var app = angular.module('myApp', []);
+
+app.controller('mainCtrl', function($scope) {
+	$scope.bars = [
+	  {
+	  	"name": "Galway",
+	  	"adresse": "13 quai des Grands Augustins,Paris"
+	  },
+	  {
+	  	"name": "Pub St Michel",
+	  	"adresse": "19 Quai Saint-Michel,Paris"
+	  },
+	  {
+	  	"name": "Le Bistrot de la Huchette",
+	  	"adresse": "27 rue de la Huchette,Paris"
+	  },
+	  {
+	  	"name": "Ze Bar",
+	  	"adresse": "20 rue de la Huchette,Paris"
+	  },
+	  {
+	  	"name": "Le Relais de la Huchette",
+	  	"adresse": "16 rue de la Huchette,Paris"
+	  },
+	  {
+	  	"name": "Latin Corner",
+	  	"adresse": "12 rue de la Huchette,Paris"
+	  },
+	  {
+	  	"name": "Georges Café",
+	  	"adresse": "4 rue de la Huchette,Paris"
+	  },
+	  {
+	  	"name": "Le petit pont",
+	  	"adresse": "1 rue de la Huchette,Paris"
+	  },
+	  {
+	  	"name": "Le Polly Maggoo",
+	  	"adresse": "5 rue du petit pont,Paris"
+	  },
+	  {
+	  	"name": "Shywawa Pub",
+	  	"adresse": "7 rue du petit pont,Paris"
+	  },
+	  {
+	  	"name": "Gentleman",
+	  	"adresse": "3 rue Hautefeuille,Paris"
+	  }
+	];
+
+
+});
+
