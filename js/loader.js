8e88db780e2a8909d5e0ac50f5fdd3015f92e310
jeremie <pro_rish@yahoo.com>
2016-05-12 15:31:40 +0200

    typo js/loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$(".clickerButton").click(function(){
-		$(".loader_page,.loader_social,#description").fadeOut(800,function(){
+		$(".home-main").fadeOut(800,function(){
 			$(".container-fluid").show();
 			console.log("ok");
 		});
